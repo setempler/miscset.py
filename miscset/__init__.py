179e43359a788786b390911ec267faf9348dcb7a
Sven E. Templer <sven.templer@gmail.com>
2021-06-02 02:39:07 +0200

    make version public

--- miscset/__init__.py
+++ miscset/__init__.py
@@ -8,12 +8,10 @@ import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
-version = (0, 1, 0)
-version_string = "{}.{}.{}".format(*version)
-
-
 from .io import Parsable
 from . import dt
 from . import io
 from . import sh
+from .version import __version__ as version
+from .version import __version_string__ as version_string
 
