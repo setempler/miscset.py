8c44b63a0ba0abc7f1a94d1dd119697293397128
Sven E. Templer <sven.templer@gmail.com>
2021-10-13 02:10:25 +0200

    version bump; fix version description

--- miscset/_version.py
+++ miscset/_version.py
@@ -1,12 +1,12 @@
 """Library version variables.
 
 The package version stored in python variables.
-Following a semantic versioning, the package version is stored as tuple (`version_` variable)
-and for convenience concatenated to a string variable (`version_string_`).
+Following a semantic versioning, the package version is stored as tuple
+and for convenience concatenated to a string variable.
 """
 
-version_tuple = (0, 2, 1, "")
-"""The (semantic) version of the module implemented as tuple with major, minor, bugfix and alpha/beta version."""
+version_tuple = (0, 2, 2, "")
+"""The (semantic) version of the module implemented as tuple with major, minor, bugfix and alpha/beta/dev version."""
 
 version = "{}.{}.{}{}".format(*version_tuple)
 """The (semantic) version of the module concatenated to a string."""
