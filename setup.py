6a2f14334a4fbc86044a60c06695b7f8a48d8d2e
Sven E. Templer <sven.templer@gmail.com>
2021-12-25 05:35:15 +0100

    setup without self-dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 import setuptools
-
-
-import miscset
-with open("README.md", "r", encoding = "utf-8") as fs:
-    long_description = fs.read()
-
+from miscset._version import version
 
 setuptools.setup(
     name = "miscset",
-    version = miscset.version,
+    version = version,
     author = "Sven Templer",
     author_email = "sven.templer@gmail.com",
     description = "Miscellaneous set of helpful methods.",
-    long_description = long_description,
+    long_description = open("README.md", encoding = "utf-8").read(),
     long_description_content_type = "text/markdown",
     url = "https://miscset.readthedocs.org",
     packages = setuptools.find_packages(),
@@ -26,6 +21,7 @@ setuptools.setup(
         "Topic :: Utilities",
         "Topic :: Text Processing :: Markup :: HTML"
     ],
-    install_requires = miscset.io.read_lines("requirements.txt"),
+    #install_requires = miscset.io.read_lines("requirements.txt"),
+    install_requires = open("requirements.txt").readlines(),
     python_requires = ">=3.6",
 )
